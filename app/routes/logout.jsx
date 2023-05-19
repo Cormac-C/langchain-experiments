c182d570a3f8b0ec3683703df10d9224b5be551e
Cormac Cureton <cormaccureton@gmail.com>
2023-05-19 13:11:38 -0400

    Remove unused routes

--- app/routes/logout.jsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "@remix-run/node";
-
-import { logout } from "~/session.server";
-
-export const action = async ({ request }) => logout(request);
-
-export const loader = async () => redirect("/");
