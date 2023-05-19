28b820b0d13bcded48c836fd76d1138898d958b9
Cormac Cureton <cormaccureton@gmail.com>
2023-05-19 12:18:25 -0400

    Initial app with open ai

---
+++ app/routes/_index.jsx
@@ -0,0 +1,41 @@
+import { Link } from "@remix-run/react";
+
+import { useOptionalUser } from "~/utils";
+
+export const meta = () => [{ title: "Remix Notes" }];
+
+export default function Index() {
+  const user = useOptionalUser();
+  return (
+    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      <div className="relative sm:pb-16 sm:pt-8">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+            <div className="absolute inset-0">
+              <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
+            </div>
+            <div className="relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pb-20 lg:pt-32">
+              <h1 className="text-center text-6xl font-extrabold tracking-tight">
+                <span className="block uppercase text-yellow-500 drop-shadow-md">
+                  Trying Langchain
+                </span>
+              </h1>
+              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+                <div className="space-y-4 sm:mx-auto sm:space-y-0">
+                  <Link
+                    to="/prompt"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                  >
+                    Try a prompt
+                  </Link>
+                  <Link to="/dashboard">Dashboard</Link>{" "}
+                  <Link to="/notes">notes</Link>{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ app/routes/prompt.jsx
@@ -0,0 +1,88 @@
+import { json } from "@remix-run/node";
+import { Form, useActionData, useNavigation } from "@remix-run/react";
+import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
+
+export async function action({ request }) {
+  const model = new OpenAI({
+    temperature: 0.7,
+  });
+
+  const template =
+    "What would be a good company name for a company that makes {product}?";
+  const prompt = new PromptTemplate({
+    template,
+    inputVariables: ["product"],
+  });
+
+  const formData = await request.formData();
+  const product = formData.get("product");
+
+  const promptText = await prompt.format({ product });
+  const res = await model.call(promptText);
+
+  return json({ result: res });
+}
+
+export default function PromptPage() {
+  const navigation = useNavigation();
+  const showLoading =
+    navigation.state === "submitting" || navigation.state === "loading";
+
+  const data = useActionData() || {};
+  let formattedResult = data?.result;
+  if (formattedResult) {
+    formattedResult = formattedResult.replaceAll("\n", "").trim();
+  }
+
+  return (
+    <div className="flex min-h-full flex-col justify-center">
+      <div className="mx-auto w-full max-w-md px-8">
+        <h2>Enter a product, receive a company name.</h2>
+      </div>
+      <div className="mx-auto w-full max-w-md px-8">
+        <Form method="post" className="space-y-6">
+          <div>
+            <label
+              htmlFor="product"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Product
+            </label>
+            <div className="mt-1">
+              <input
+                id="product"
+                required
+                name="product"
+                type="text"
+                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              />
+            </div>
+          </div>
+          <button
+            disabled={showLoading}
+            type="submit"
+            className="w-full rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
+          >
+            {showLoading ? "Submitting..." : "Submit"}
+          </button>
+        </Form>
+      </div>
+      <div className="mx-auto my-4 w-full max-w-md px-8">
+        <label className="block text-sm font-medium text-gray-700">
+          Output
+        </label>
+        <div className="mt-1">
+          <textarea
+            id="output"
+            name="output"
+            className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+            rows={10}
+            readOnly
+            value={formattedResult || ""}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
